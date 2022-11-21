cd91ad988675f4d9a479f9dfde569b7ff4b8a8c2
colony00 <zeohye00@gmail.com>
2022-11-21 18:09:14 +0100

    Added a gif display of the weather aswell as text of temperature.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const WEATHER_API_KEY = "38a6285835f70040b63d91a7cea40a1d";
+const GIF_API_KEY = "qCKx9YlFhV69ob7gIspPEJVbaBCZgziv";
 
 async function reqWeatherData (location) {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${WEATHER_API_KEY}`);
@@ -17,6 +18,19 @@ async function getWeatherData (location,celscius=true) {
   return {weatherType,temp}
 }
 
+async function reqGiphy (weather) {
+  const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${GIF_API_KEY}&s=${weather}`);
+  console.log(`https://api.giphy.com/v1/gifs/translate?api_key=${GIF_API_KEY}&s=${weather}`);
+  return await response.json();
+}
+
+async function showWeatherGif (weather) {
+  const json = await reqGiphy(weather);
+  const url = json.data.images.original.url;
+  const img = document.querySelector("img");
+  img.src = url;
+}
+
 const form = document.querySelector("form");
 const loc = document.querySelector("#loc");
 
@@ -26,12 +40,11 @@ form.addEventListener("submit", e => {
   getWeatherData(location)
     .then( 
       function fulfilled ( {weatherType, temp} ) {
-        console.log(weatherType,temp)
-      },
-  
-      function rejected(err) {
-        console.log("Failed with error:",err)
+        const tempDOM = document.querySelector(".temp");
+        tempDOM.textContent = temp;
+        showWeatherGif(weatherType);
+        console.log(weatherType,temp);
       }
-    );
+    ).catch( err => console.log("Quited with error:",err))
   e.preventDefault();
 })
